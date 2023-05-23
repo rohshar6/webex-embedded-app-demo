7e5d141e136bf202a8408e1fe193f942809b322b
Kesava Krishnan Madavan <kmadavan@cisco.com>
2023-05-23 18:29:37 +0530

    feat(sidebar): embedded app sdk demo

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+let callInfo, sidebar;
+window.onload = async () => {
+    const app = new window.webex.Application();
+    await app.onReady();
+    sidebar = await app.context.getSidebar();
+    app.listen().then(() => {
+        app.on("sidebar:callStateChanged", handleCallStateChange);
+        app.on("application:viewStateChanged", handleViewStateChange)
+    }).catch((reason) => {
+        console.error("listen: fail reason=" + webex.Application.ErrorCodes[reason]);
+    });
+}
+
+async function handleCallStateChange(call) {
+    callInfo = call;
+    switch (call.state) {
+      case "Started":
+        console.log("A call has come in...");
+        await sidebar.showBadge({
+            badgeType: 'count',
+            count: 1
+        });
+        break;
+    }
+}
+
+async function handleViewStateChange(viewState){
+    if(viewState === 'IN_FOCUS'){
+        await sidebar.clearBadge();
+        if(callInfo){
+            window.location.href = `https://directory.cisco.com/dir/empquery.go?q=2002`
+        }
+    }
+}
